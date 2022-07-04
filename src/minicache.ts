3f088979ba26dc028d1230386f244f3087fc6031
Gustavo Cavalcante <nowayhecodes@gmail.com>
2022-07-04 00:23:18 -0300

    chore: use pnpm

--- src/minicache.ts
+++ src/minicache.ts
@@ -1,27 +1,27 @@
 import "reflect-metadata";
 import { CACHE_MANAGER, Inject } from "@nestjs/common";
 
-type Inject = (target: object, key: string | symbol, index?: number) => void;
+type TInject = (target: object, key: string | symbol, index?: number) => void;
 
 export const MiniCache = (ttl = 10) => {
-  const injectCache = Inject(CACHE_MANAGER);
+  const injectCache: TInject = Inject(CACHE_MANAGER);
   return function (
     target: any,
     _propertyName: string,
     descriptor: PropertyDescriptor
   ) {
-    injectCache(target, "cache");
+    injectCache(target, "minicache");
     const decoratedMethod = descriptor.value;
     const cacheKey = `${target.constructor.name}-${decoratedMethod?.name}`;
 
     descriptor.value = async function () {
-      const cachedData = await this.cache.get(cacheKey);
+      const cachedData = await this.minicache.get(cacheKey);
       if (cachedData) {
         return cachedData;
       }
 
       const response = await decoratedMethod.apply(this, arguments);
-      this.cache.set(cacheKey, response, { ttl: ttl });
+      this.minicache.set(cacheKey, response, { ttl: ttl });
       return response;
     };
   };
